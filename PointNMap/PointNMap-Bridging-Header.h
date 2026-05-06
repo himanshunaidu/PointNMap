ddfb177dc10aba5cacaf3e91236ad66f6b3d63d5
himanshunaidu <himanshunaidu12161997@gmail.com>
2026-05-06 16:55:04 -0700

    Add dependencies and other build settings to the demo app

---
+++ PointNMap/PointNMap-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  PointNMap-Bridging-Header.h
+//  PointNMap
+//
+//  Created by Himanshu on 5/6/26.
+//
+
+#ifndef PointNMap_Bridging_Header_h
+#define PointNMap_Bridging_Header_h
+
+
+#endif /* PointNMap_Bridging_Header_h */
+
+#import "ShaderTypes.h"
