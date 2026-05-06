31d5da51e88caee50d03005634318bfb33c5a421
himanshunaidu <himanshunaidu12161997@gmail.com>
2026-05-06 15:34:41 -0700

    Initial Commit

---
+++ PointNMap/PointNMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointNMapApp.swift
+//  PointNMap
+//
+//  Created by Himanshu on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct PointNMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
